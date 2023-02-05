92a4feb2b43056b1dcb4a05daa6ce802611f7ef2
Cody <codydavid51@aol.com>
2023-02-05 01:17:30 -0600

    feat: directory movement

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import {Home} from '../Pages/Home'
+import {Home} from './Pages/Home'
 
 function App() {
   return (
